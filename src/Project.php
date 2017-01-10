9cd48dc43254f442a70bdc0abf4716baa5733701
methylbro <thomas@gasc.fr>
2017-01-10 08:23:14 +0100

    autodetect version from git

--- src/Project.php
+++ src/Project.php
@@ -35,7 +35,7 @@ class Project
 
 	public function compile($path, $pharName, $manifest)
 	{
-        $extra = array('version' => 'dev-master');
+        $extra = ['version' => $this->detectVersion($path)];
 
         if (file_exists($path.DIRECTORY_SEPARATOR.$manifest)) {
             $this->logger->notice('Using '.$manifest);
@@ -64,4 +64,40 @@ class Project
             $this->compiler->execute($path, $pharName);
         }
 	}
+
+    private function detectVersion($path)
+    {
+        $version = 'dev-master';
+        $gitdir = $path.DIRECTORY_SEPARATOR.'.git';
+        
+        if (file_exists($gitdir)) {
+            $c = "git --git-dir={$gitdir} show -q";
+            $show = shell_exec($c);
+            $commit = explode(PHP_EOL, $show)[0];
+            $commit = substr($commit, 7);
+
+            if ($commit) {
+                $c = "git --git-dir={$gitdir} tag --contains {$commit}";
+                $tags = shell_exec($c);
+                $tags = explode(PHP_EOL, $tags);
+                $tag = $tags[0];
+
+                if ($tag) {
+                    $version = $tag;
+                } else {
+                    $c = "git --git-dir={$gitdir} branch";
+                    $branch = shell_exec($c);
+                    $branch = explode(PHP_EOL, $branch);
+                    foreach ($branch as $b) {
+                        if (substr($b, 0, 1)==='*') {
+                            $branch = trim($b, '* ');
+                        }
+                    }
+                    $version = 'dev-'.$branch;
+                }
+            }
+        }
+
+        return $version;
+    }
 }
